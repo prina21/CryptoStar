ed6655d655de21d7952767db5b838e89c75939b4
Prina Gudhka <prinagudhka@dhcp-10-8-044-096.mobile.reshsg.uci.edu>
2025-04-16 19:01:52 -0700

    place order

---
+++ oms/alpaca_client.py
@@ -0,0 +1,25 @@
+from alpaca_trade_api.rest import REST
+
+class AlpacaClient:
+    def __init__(self, api_key, secret_key, base_url):
+        self.api = REST(api_key, secret_key, base_url)
+
+    def submit_market_order(self, symbol, qty, side):
+        return self.api.submit_order(
+            symbol=symbol,
+            qty=qty,
+            side=side,
+            type="market",
+            time_in_force="gtc"
+        )
+
+    def submit_limit_order(self, symbol, qty, side, limit_price):
+        return self.api.submit_order(
+            symbol=symbol,
+            qty=qty,
+            side=side,
+            type="limit",
+            limit_price=limit_price,
+            time_in_force="gtc"
+        )
+
