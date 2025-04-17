14c5181409531dd1f7df0dbed90f7ac13dc1b4d1
Prina Gudhka <prinagudhka@dhcp-10-8-044-096.mobile.reshsg.uci.edu>
2025-04-16 18:34:15 -0700

    Order class and enums

---
+++ oms/order.py
@@ -0,0 +1,35 @@
+from enum import Enum
+import uuid
+import time
+
+
+class Side(Enum):
+    BUY = "buy"
+    SELL = "sell"
+
+
+class OrderType(Enum):
+    MARKET = "market"
+    LIMIT = "limit"
+
+
+class OrderStatus(Enum):
+    SUBMITTED = "SUBMITTED"
+    FILLED = "FILLED"
+    CANCELLED = "CANCELLED"
+    REJECTED = "REJECTED"
+
+
+class Order:
+    def __init__(self, symbol, qty, side: Side, type_: OrderType, limit_price=None):
+        self.id = str(uuid.uuid4())
+        self.timestamp = time.time()
+        self.symbol = symbol
+        self.qty = qty
+        self.side = side
+        self.type = type_
+        self.limit_price = limit_price
+        self.status = OrderStatus.SUBMITTED
+
+    def __repr__(self):
+        return f"<Order {self.id[:6]} | {self.side.name} {self.qty} {self.symbol} @ {self.limit_price or 'market'}>"
--- orders/oms.py
+++
@@ -1,2 +0,0 @@
-def validate_order(order):
-    print("Validate order")
