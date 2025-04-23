190f58896d6ada198d698a5ff00d17cc0f34e19e
MitaliD <mtldeshmukh@gmail.com>
2025-04-23 13:21:06 -0700

    conflict

--- backend/app/engine/config/settings.py
+++ backend/app/engine/config/settings.py
@@ -1,5 +1,5 @@
-ALPACA_API_KEY = "PKZSNL7EP99TJMMQ9UV8"
-ALPACA_SECRET_KEY = "yJIwABAvJmhryYH4iiEG5hdkaUk3gVWqdbhLL0xw"
+ALPACA_API_KEY = ""
+ALPACA_SECRET_KEY = ""
 ALPACA_BASE_URL = "https://paper-api.alpaca.markets"
 ALPACA_STREAM_URL = "wss://stream.data.alpaca.markets/v1beta1/crypto"
 TA_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjgwMDYzMTA4MDZmZjE2NTFlNWY2YTI4IiwiaWF0IjoxNzQ0ODU1ODI0LCJleHAiOjMzMjQ5MzE5ODI0fQ._ot5NyppEhzcp28R-PNyxl2gPtpBq2IUFj_sTzukgF4"
